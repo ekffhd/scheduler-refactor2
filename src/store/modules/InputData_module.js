9b8afe41deb912c177c6b8ee7724d6f170088461
James <jamesj0107@naver.com>
2019-02-04 16:50:45 +0900

    Check for timetable overlap

--- src/store/modules/InputData_module.js
+++ src/store/modules/InputData_module.js
@@ -13,19 +13,58 @@ const mutations = {
         let result = state.pin_lectures.findIndex(lecture => lecture.code === subject.code);
         if(result === -1){
             result = state.option_lectures.findIndex(lecture => lecture.code === subject.code);
-            if(result === -1){
+
+            if(result !== -1){
+                alert('해당 고정강의는 이미 선택강의로 추가되었습니다.');
+                subject.out = {
+                    status : "failed"
+                };
+                return;
+            }
+
+            let overlap = false;
+
+            for (let i=0; i<state.pin_lectures.length; i++){
+
+                for (let j=0; j<state.pin_lectures[i].timetable.length; j++){
+
+                    let pin_lecture_time = state.pin_lectures[i].timetable[j];
+
+                    const pin_start = pin_lecture_time.start.replace(/:/gi, '');
+                    const pin_end = pin_lecture_time.end.replace(/:/gi, '');
+
+                    for(let k=0; k<subject.timetable.length; k++){
+                        let subject_time = subject.timetable[k];
+
+                        if (pin_lecture_time.day !== subject_time.day) continue;
+
+                        const subject_start = subject_time.start.replace(/:/gi, '');
+                        const subject_end = subject_time.end.replace(/:/gi, '');
+
+                        if ((pin_start < subject_end && pin_end > subject_start) || (pin_end > subject_start && pin_end < subject_end)){
+                            overlap = true;
+                            break;
+                        }
+                    }
+                    if (overlap === true) break;
+                }
+                if (overlap === true) break;
+            }
+
+            if (overlap !== true){
                 state.pin_lectures.push(subject);
                 subject.out = {
                     status : "succeed"
                 }
             }
-            else{
-                alert('해당 학수번호는 이미 선택강의에 추가되었습니다.');
+            else {
+                alert('해당 시간표에 이미 고정강의가 추가되었습니다.');
                 subject.out = {
                     status : "failed"
                 }
             }
-        }else{
+        }
+        else{
             alert('해당 학수번호는 이미 고정강의에 추가되었습니다.');
             subject.out = {
                 status : "failed"
